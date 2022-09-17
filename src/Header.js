e90d3ebce463ae7f0dd373b4777d465114273d77
Zach Byerley <zachbyerley@gmail.com>
2022-09-16 23:26:04 -0400

    BUG: cant create new obstacles that move from left to right

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
     return (
         <header>
-            <h1>Blocker</h1>
+            <h1>Blockger</h1>
         </header>
     );
 }
--- src/game/Game.js
+++ src/game/Game.js
@@ -1,17 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+//default position for player
 const defaultPos = { top: '80vh', left: '48vw' };
+
+//helper function to animate obstacles
+function createAndMove(freq, obstacle, parent) {
+    //TODO: set an interval that creates obstacles and changes stlye left to end of the page which will cause movement
+    // console.log('car');
+    setInterval(() => {
+        //this c-l triggers only after saving and clicking somewhere else.....what?
+        console.log('wut');
+        //rest of this doesnt trigger
+        const obst = document.createElement('div');
+        obst.className = obstacle;
+        obst.style = { left: '300vh' };
+        parent.appendChild(obst);
+    }, freq);
+}
 
 export default function Game() {
     const [position, setPosition] = useState(defaultPos);
 
+    const streets = Array.from(document.getElementsByClassName('street'));
+    const logs = Array.from(document.getElementsByClassName('water'));
+
+    if (streets)
+        streets.forEach(street => {
+            createAndMove(3000, 'car', street);
+        });
+
+    //Player movement
     function handleKey(event) {
+        const moveLeft = 2;
         const top = Number(position.top.slice(0, -2));
         const left = Number(position.left.slice(0, -2));
         switch (event.key) {
             case 'ArrowUp':
             case 'w':
             case 'W':
+                //TODO: change 'if' for winning
+                //set pos to start, increase points
                 if (top - 10 < 10) break;
                 setPosition({ ...position, top: `${top - 10}vh` });
                 break;
@@ -24,14 +52,14 @@ export default function Game() {
             case 'ArrowLeft':
             case 'a':
             case 'A':
-                if (left - 4 < 0) break;
-                setPosition({ ...position, left: `${left - 4}vw` });
+                if (left - moveLeft < 0) break;
+                setPosition({ ...position, left: `${left - moveLeft}vw` });
                 break;
             case 'ArrowRight':
             case 'd':
             case 'D':
-                if (left + 4 > 97) break;
-                setPosition({ ...position, left: `${left + 4}vw` });
+                if (left + moveLeft > 96) break;
+                setPosition({ ...position, left: `${left + moveLeft}vw` });
                 break;
             default:
                 break;
@@ -42,12 +70,22 @@ export default function Game() {
         <div id="game-board" tabIndex={0} onKeyDown={handleKey}>
             <div id="player" style={position}></div>
             <div className="row grass"></div>
-            <div className="row water"></div>
-            <div className="row water"></div>
+            <div className="row water">
+                <div className="log"></div>
+            </div>
+            <div className="row water">
+                <div className="log"></div>
+            </div>
             <div className="row grass"></div>
-            <div className="row street"></div>
-            <div className="row street"></div>
-            <div className="row street"></div>
+            <div className="row street">
+                <div className="car"></div>
+            </div>
+            <div className="row street">
+                <div className="bus"></div>
+            </div>
+            <div className="row street">
+                <div className="car"></div>
+            </div>
             <div className="row grass"></div>
         </div>
     );
